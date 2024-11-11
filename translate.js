9b2cef2e033ffd2fbd73aed5bdaa9848811dbe08
Alan Arzumanjan <alanarzumanjan@gmail.com>
2024-11-11 10:57:00 +0200

    First upload

---
+++ translate.js
@@ -0,0 +1,105 @@
+const translations = {
+    'en': {
+        'home': 'Home',
+        'shop': 'Shop',
+        'info': 'Info',
+        'contacts': 'Contacts',
+        'info-text': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit sapiente amet consectetur vitae corporis, temporibus velit nesciunt illo debitis ab qui officia dolores unde repellendus voluptas possimus consequuntur nostrum! Dolore?Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur debitis reprehenderit perferendis eveniet, molestias ab eaque veniam officia, hic cum consequuntur ad neque qui. Animi delectus dignissimos recusandae quisquam nesciunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus laudantium excepturi aperiam architecto, animi optio distinctio ex, aspernatur cumque fugit quis voluptatibus, quidem obcaecati quod atque repellendus molestias accusamus sed! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam sapiente architecto consequuntur porro reprehenderit natus enim cum perferendis. Minus, doloribus necessitatibus cumque perferendis architecto ut repudiandae voluptatem delectus dicta ad!',
+        'company-name': 'Meltwater Company',
+        'additional-info': 'We offer high quality water and many other products for your health and comfort.',
+        'best-offers': 'Best offers',
+        'product-title': 'Meltwater',
+        'product-description': 'The product description will be here',
+        'add-to-cart': 'Add to Cart',
+        'info-title': 'Information',
+        'info-text-forcompany': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga aut sequi dolor consequatur obcaecati ex iste consequuntur tenetur in, voluptatem, facere eos quod expedita error, sapiente similique. Odio, atque quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ut beatae sequi temporibus dolorem reiciendis corrupti distinctio minus ipsum aperiam, accusantium illum mollitia perspiciatis! Fugit, nam! Voluptatem ratione consectetur maiores. Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        'our-social-networks': 'Our social networks',
+        'social-media': 'You can also follow us on social media:',
+
+        'Contact-information': 'Contact information',
+        'Address': 'Address:',
+        'Phone': 'Phone:',
+        'Email': 'Email:',
+        'Feedback-form': 'Feedback form',
+        'Comments': 'If you have any questions or comments, please fill out the feedback form below:',
+        'Your-name': 'Your name:',
+        'Your-email': 'Your email:',
+        'message': 'Email:',
+        'submit': 'Submit',
+
+    },
+    'lv': {
+        'home': 'Sākums',
+        'shop': 'Veikals',
+        'info': 'Informācija',
+        'contacts': 'Kontakti',
+        'info-text': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit sapiente amet consectetur vitae corporis, temporibus velit nesciunt illo debitis ab qui officia dolores unde repellendus voluptas possimus consequuntur nostrum! Dolore?Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur debitis reprehenderit perferendis eveniet, molestias ab eaque veniam officia, hic cum consequuntur ad neque qui. Animi delectus dignissimos recusandae quisquam nesciunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus laudantium excepturi aperiam architecto, animi optio distinctio ex, aspernatur cumque fugit quis voluptatibus, quidem obcaecati quod atque repellendus molestias accusamus sed! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam sapiente architecto consequuntur porro reprehenderit natus enim cum perferendis. Minus, doloribus necessitatibus cumque perferendis architecto ut repudiandae voluptatem delectus dicta ad!',
+        'company-name': 'Udens uzņēmums Meltwater',
+        'additional-info': 'Mēs piedāvājam augstas kvalitātes ūdeni un daudzus citus produktus jūsu veselībai un komfortam.',
+        'best-offers': 'Labākie piedāvājumi',
+        'product-title': 'Meltwater',
+        'product-description': 'Produkta apraksts būs šeit',
+        'add-to-cart': 'Pievienot grozam',
+        'info-title': 'Informācija',
+        'info-text-forcompany': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga aut sequi dolor consequatur obcaecati ex iste consequuntur tenetur in, voluptatem, facere eos quod expedita error, sapiente similique. Odio, atque quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ut beatae sequi temporibus dolorem reiciendis corrupti distinctio minus ipsum aperiam, accusantium illum mollitia perspiciatis! Fugit, nam! Voluptatem ratione consectetur maiores. Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        'our-social-networks': 'Mūsu sociālie tīkli',
+        'social-media': 'Varat mums sekot arī sociālajos plašsaziņas līdzekļos:',
+
+        'Contact-information': 'Kontaktinformācija',
+        'Address': 'Adrese:',
+        'Phone': 'Tālrunis:',
+        'Email': 'E-pasts:',
+        'Feedback-form': 'Atsauksmes veidlapa',
+        'Comments': 'Ja jums ir kādi jautājumi vai komentāri, lūdzu, aizpildiet atsauksmju veidlapu zemāk:',
+        'Your-name': 'Jūsu vārds:',
+        'Your-email': 'Jūsu e-pasta adrese:',
+        'message': 'Vēstule:',
+        'submit': 'Sūtīt',
+
+    },
+    'rs': {
+        'home': 'Главная',
+        'shop': 'Магазин',
+        'info': 'Информация',
+        'contacts': 'Контакты',
+        'info-text': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit sapiente amet consectetur vitae corporis, temporibus velit nesciunt illo debitis ab qui officia dolores unde repellendus voluptas possimus consequuntur nostrum! Dolore?Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur debitis reprehenderit perferendis eveniet, molestias ab eaque veniam officia, hic cum consequuntur ad neque qui. Animi delectus dignissimos recusandae quisquam nesciunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus laudantium excepturi aperiam architecto, animi optio distinctio ex, aspernatur cumque fugit quis voluptatibus, quidem obcaecati quod atque repellendus molestias accusamus sed! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam sapiente architecto consequuntur porro reprehenderit natus enim cum perferendis. Minus, doloribus necessitatibus cumque perferendis architecto ut repudiandae voluptatem delectus dicta ad!',
+        'company-name': 'Компания Meltwater',
+        'additional-info': 'Мы предлагаем высококачественную воду и множество других продуктов для вашего здоровья и комфорта.',
+        'best-offers': 'Лучшие предложения',
+        'product-title': 'Meltwater',
+        'product-description': 'Описание продукта будет здесь',
+        'add-to-cart': 'Добавить в корзину',
+        'info-title': 'Информация',
+        'info-text-forcompany': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga aut sequi dolor consequatur obcaecati ex iste consequuntur tenetur in, voluptatem, facere eos quod expedita error, sapiente similique. Odio, atque quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ut beatae sequi temporibus dolorem reiciendis corrupti distinctio minus ipsum aperiam, accusantium illum mollitia perspiciatis! Fugit, nam! Voluptatem ratione consectetur maiores. Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        'our-social-networks': 'Наши социальные сети',
+        'social-media': 'Вы также можете следить за нами в социальных сетях:',
+
+        'Contact-information': 'Контактная информация',
+        'Address': 'Адресс:',
+        'Phone': 'Телефон:',
+        'Email': 'Э-почта:',
+        'Feedback-form': 'Форма обратной связи',
+        'Comments': 'Если у Вас есть вопросы или комментарии, пожалуйста, заполните форму обратной связи ниже:',
+        'Your-name': 'Ваше имя:',
+        'Your-email': 'Ваша электронная почта:',
+        'message': 'Письмо:',
+        'submit': 'Отправить',
+
+    },
+};
+
+function setLanguage(language) {
+    const elements = document.querySelectorAll('[data-translate]');
+    elements.forEach(element => {
+        const key = element.getAttribute('data-translate');
+        element.textContent = translations[language][key];
+    });
+}
+
+const languageSelect = document.getElementById('language-select');
+languageSelect.addEventListener('change', function () {
+    const selectedLanguage = languageSelect.value;
+    setLanguage(selectedLanguage);
+});
+
+setLanguage(languageSelect.value);
